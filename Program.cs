0a9645427dd07c25bf3618cf60e548c4a3fbef2f
Khaled  Assi <97214065+assikhaled20@users.noreply.github.com>
2023-02-26 22:38:39 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CSExercises;
+class HelloWorld
+{
+    static void Main()
+    {
+
+Console.WriteLine("Hello, World!");
+  
+    }}
